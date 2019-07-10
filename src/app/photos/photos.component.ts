20749854f0b718a81d70c161b8f98414c21233fb
pfauchet <42136778+pfauchet@users.noreply.github.com>
2019-07-10 15:20:29 +0200

    Added hashtag links

--- src/app/photos/photos.component.ts
+++ src/app/photos/photos.component.ts
@@ -12,15 +12,22 @@ export class PhotosComponent implements OnInit {
   constructor(private instagramService: InstagramService) { }
 
   ngOnInit() {
-    this.instagramService.getLatestInstagramPosts((data)=> {
+    this.instagramService.getLatestInstagramPosts((data) => {
       this.instagramPosts = data.data;
       this.instagramPosts.forEach(element => {
         if (element.media_type != "IMAGE")
-          element.displayPost=false;
-        else 
-          element.displayPost=true;
+          element.displayPost = false;
+        else
+          element.displayPost = true;
       });
     })
   }
 
+  ngAfterViewChecked() {
+    var elements = document.getElementsByClassName("instagram-caption");
+    for (var i = 0; i < elements.length; i++) {
+      var text = elements[i].innerHTML
+      elements[i].innerHTML = text.replace(/#([A-Za-zÀ-ÖØ-öø-ÿ0-9]+)/g, '<a href="https://www.instagram.com/explore/tags/$1" target="_blank" rel="noopener noreferrer">#$1</a>');
+    }
+  }
 }
