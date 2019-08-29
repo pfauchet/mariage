356242179885875a6d41e9d1073d48edb84f3055
pfauchet <42136778+pfauchet@users.noreply.github.com>
2019-08-29 11:29:59 +0200

    Added loading icon when submitting confirmation

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -17,6 +17,7 @@ export class ConfirmationComponent implements OnInit {
 
   displaySecondForm: boolean = false;
   displayErrorMessage: boolean = false;
+  displayLoader: boolean = false;
   displayForms: boolean = true;
   errorMessage: string;
   successMessage: string = null;
@@ -34,25 +35,33 @@ export class ConfirmationComponent implements OnInit {
     console.log("Searching attendant");
     this.displayErrorMessage = false;
     this.displaySecondForm = false;
+    this.displayLoader = true;
 
     if (!this.surname || !this.name) {
       this.errorMessage = "Merci de compléter tous les champs";
       this.displayErrorMessage = true;
+      this.displayLoader = false;
     }
     else {
       this.confirmationService.searchAttendant(this.name, this.surname, (err, data) => {
         if (err) {
           console.log(err);
+          this.errorMessage = "Une erreur est survenue, veuillez réessayer plus tard.";
+          this.displayErrorMessage = true;
+          this.displayLoader = false;
         } else {
           if (data.errorMessage == "NOT_FOUND") {
             this.errorMessage = "Invité non trouvable";
             this.displayErrorMessage = true;
+            this.displayLoader = false;
           } else if (data.errorMessage) {
             this.errorMessage = "Une erreur est survenue, veuillez réessayer plus tard.";
             this.displayErrorMessage = true;
+            this.displayLoader = false;
           } else {
             this.attendantData = data;
             this.displaySecondForm = true;
+            this.displayLoader = false;
           }
         }
       })
@@ -62,32 +71,35 @@ export class ConfirmationComponent implements OnInit {
   onConfirm() {
     console.log("Confirmation en cours");
     this.displayErrorMessage = false;
+    this.displayLoader = true;
 
     this.confirmationService.confirmReservation(this.attendantData.code, this.attendantData.token, this.isAttending, this.isWithPlusOne, this.isWithChildren, this.email, (err, data) => {
       if (err) {
         console.log("Erreur lors de la confirmation", err);
         this.displayErrorMessage = true;
         this.errorMessage = "Oops, une erreur est survenue lors de la confirmation de ta venue ...";
+        this.displayLoader = false;
       }
       else {
         if (data.errorMessage) {
           if (data.errorMessage == "INVALID_EMAIL") {
             this.errorMessage = "Format de l'email invalide.";
             this.displayErrorMessage = true;
+            this.displayLoader = false;
           }
           else {
             this.errorMessage = "Une erreur est survenue, veuillez réessayer plus tard.";
             this.displayErrorMessage = true;
+            this.displayLoader = false;
           }
         }
         else {
           console.log("Confirmation réussie", data);
           this.successMessage = "Confirmation réussie, merci pour votre réponse."
           this.displayForms = false;
+          this.displayLoader = false;
         }
       }
     })
-
   }
-
 }
