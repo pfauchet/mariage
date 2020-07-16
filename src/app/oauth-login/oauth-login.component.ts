e80db2b0d4224dc8c73c4c1120fb1adc5fa4703f
pfauchet <42136778+pfauchet@users.noreply.github.com>
2020-07-16 15:01:06 +0200

    Patched redirect URI

--- src/app/oauth-login/oauth-login.component.ts
+++ src/app/oauth-login/oauth-login.component.ts
@@ -10,7 +10,7 @@ export class OauthLoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    var redirectUri = location.origin + 'oauth-redirect';
+    var redirectUri = location.origin + '/oauth-redirect';
     
     window.location.href = 'https://www.facebook.com/v3.3/dialog/oauth?client_id=478083806341667&redirect_uri=' +  redirectUri +'&state=toto&response_type=code&scope=instagram_basic manage_pages';
   }
